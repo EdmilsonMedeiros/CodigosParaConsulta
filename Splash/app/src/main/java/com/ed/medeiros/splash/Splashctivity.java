1b623250adceb60e8675d3a50190fcec7e695db0
EdmilsonMedeiros <edmilson.mprojetos@gmail.com>
2020-10-22 21:59:49 -0300

    Splash

---
+++ Splash/app/src/main/java/com/ed/medeiros/splash/Splashctivity.java
@@ -0,0 +1,30 @@
+package com.ed.medeiros.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class Splashctivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashctivity);
+
+
+        //FULLSCREEN:
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //Redireciona após 2s para PrincipalActivity:
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splashctivity.this, SegundaActivity.class));
+            }
+        }, 2000);
+
+    }
+}
