32ed6e2df873e6b1eeb34313e745b7ce9497cc27
Edmilson Medeiros <edmilson.mprojetos@gmail.com>
2022-01-24 21:39:05 -0300

    CalendarView

---
+++ Calendario/app/src/main/java/com/example/calendario/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.calendario;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.CalendarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CalendarView calendarView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calendarView = findViewById(R.id.calendarView);
+
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                //Os meses iniciam em zero(adicionado month + 1)
+                Log.i("data: ", dayOfMonth + "/" + (month + 1) + "/" + year);
+            }
+        });
+
+    }
+}
